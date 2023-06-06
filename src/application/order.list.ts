9396041331b44388653f9d0a94eefcd356b45d05
Philippe <philippe.3.lemaire@atos.net>
2023-06-06 09:25:54 +0200

    chore: ajout Readme

--- src/application/order.list.ts
+++
@@ -1,5 +0,0 @@
-export class OrderList {
-  exec (array: number[]): number[] {
-    return array.sort((a, b) => a - b)
-  }
-}
--- test/merge.sort.spec.ts
+++
@@ -1,7 +0,0 @@
-describe('merge sort', () => {
-  it('should sort an array of numbers', () => {
-    const array = [1, 5, 3, 2, 4]
-    const sorted = [1, 2, 3, 4, 5]
-    expect(array.sort((a, b) => a - b)).toEqual(sorted)
-  })
-})
